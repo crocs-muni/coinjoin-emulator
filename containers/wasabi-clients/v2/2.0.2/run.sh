0e5319e1bd56e31f3e06022ed71d9ae0f70300d3
davedr <david.184@seznam.cz>
2024-03-14 18:28:48 +0100

    add image info for version 2.0.2

---
+++ containers/wasabi-clients/v2/2.0.2/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$ADDR_BTC_NODE" ]; then
+    export ADDR_BTC_NODE="btc-node"
+fi
+if [ -z "$ADDR_WASABI_BACKEND" ]; then
+    export ADDR_WASABI_BACKEND="wasabi-backend"
+fi
+mkdir -p /home/wasabi/.walletwasabi/client
+( echo "cat <<EOF" ; cat /home/wasabi/Config.json ; echo EOF ) | sh > /home/wasabi/.walletwasabi/client/Config.json
+./WalletWasabi.Fluent.Desktop
